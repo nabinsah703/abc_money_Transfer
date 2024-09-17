a097f421d3db720edf8a03a8c363bf07e7bf6a62
Navin Sah <nabinsah703@gmail.com>
2024-09-17 21:54:07 +0545

    creating project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ABC_Money_Transfer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
